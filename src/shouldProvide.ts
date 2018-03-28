db47e6057fb5c868c9238cbd60a52de26ea4e06d
manishb <xmbhasin@gmail.com>
2018-03-27 22:42:02 -0400

    Renamed regex constants in src/shouldProvide.ts

--- src/shouldProvide.ts
+++ src/shouldProvide.ts
@@ -1,21 +1,29 @@
 import * as vscode from 'vscode';
 
-export function shouldProvide(document: vscode.TextDocument, position: vscode.Position): boolean {
-  return isMixfile(document.fileName) && isCursorInDepsBlock(document, position);
+export function shouldProvide(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): boolean {
+  return (
+    isMixfile(document.fileName) && isCursorInDepsBlock(document, position)
+  );
 }
 
 function isMixfile(fileName: String): boolean {
   return fileName.endsWith('mix.exs');
 }
 
-function isCursorInDepsBlock(document: vscode.TextDocument, position: vscode.Position): boolean {
+function isCursorInDepsBlock(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): boolean {
   // find deps function definition 'defp deps do'
   const fileStartPosition = new vscode.Position(0, 0);
   const leftRange = new vscode.Range(fileStartPosition, position);
   const leftText = document.getText(leftRange);
 
-  const depsBlockRegex = /def[p]?[\s]+deps[\s]+do$/m; //assumes there is only one `deps` function
-  const indexOfDepsHead = leftText.search(depsBlockRegex);
+  const depsHeadRegex = /def[p]?[\s]+deps[\s]+do$/m; //assumes there is only one `deps` function
+  const indexOfDepsHead = leftText.search(depsHeadRegex);
   // console.log(indexOfDepsHead);
   if (indexOfDepsHead <= -1) {
     return false;
@@ -24,8 +32,9 @@ function isCursorInDepsBlock(document: vscode.TextDocument, position: vscode.Pos
   const depsHeadToCursor = leftText.substr(indexOfDepsHead);
   // console.log(depsHeadToCursor);
 
-  const depsBlockEndRegex = /^[\s]*end$/m;
-  if (depsHeadToCursor.search(depsBlockEndRegex) > -1) { //assumes `end` does not appear by itself in a line in deps block
+  const depsEndRegex = /^[\s]*end$/m;
+  if (depsHeadToCursor.search(depsEndRegex) > -1) {
+    //assumes `end` does not appear by itself in a line in deps block
     // console.log("cursor NOT in deps block");
     return false;
   }
