23cba77141faef725a68587bdfd38a0248d421eb
manishb <xmbhasin@gmail.com>
2018-04-07 11:21:54 -0400

    Remove debug logging

--- src/shouldProvide.ts
+++ src/shouldProvide.ts
@@ -25,19 +25,15 @@ function isCursorInDepsBlock(
   const depsHeadRegex = /def[p]?[\s]+deps[\s]+do$/m; // assumes there is only one `deps` function
   const indexOfDepsHead = leftText.search(depsHeadRegex);
   if (indexOfDepsHead <= -1) {
-    // console.log("cursor NOT in deps block");
     return false;
   }
 
   const depsHeadToCursor = leftText.substr(indexOfDepsHead);
-  // console.log(depsHeadToCursor);
   const depsEndRegex = /^[\s]*end$/m;
   if (depsHeadToCursor.search(depsEndRegex) > -1) {
     // assumes `end` does not appear by itself in a line in deps block
-    // console.log("cursor NOT in deps block");
     return false;
   }
 
-  // console.log("cursor in deps block");
   return true;
 }
